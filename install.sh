3dbb39ae179d048973441d76536cb8517caed994
kenneth koontz <kenneth.koontz@gmail.com>
2012-04-26 20:23:39 -0700

    Added install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+ln -sf /home/kenneth/.dotfiles/.gitconfig /home/kenneth/.gitconfig
+ln -sf /home/kenneth/.dotfiles/.vimrc /home/kenneth/.vimrc
+ln -sf /home/kenneth/.dotfiles/.bashrc /home/kenneth/.bashrc
+ln -sf /home/kenneth/.dotfiles/.profile /home/kenneth/.profile
