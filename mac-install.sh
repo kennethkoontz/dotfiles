d37828a8fdc905f79d5d489ad833f1f4949a83a1
kenneth koontz <kenneth.koontz@gmail.com>
2014-07-09 11:36:22 -0700

    update mac-install script

--- mac-install.sh
+++ mac-install.sh
@@ -7,3 +7,11 @@ fi
 if ln -sf $PWD/.vimrc $HOME/.vimrc ; then
 	echo created symlink: $PWD/.vimrc $HOME/.vimrc
 fi
+
+if ln -sf $PWD/.tmux.conf $HOME/.tmux.conf ; then
+	echo created symlink: $PWD/.tmux.conf $HOME/.tmux.conf
+fi
+
+if ln -sf $PWD/.zshrc $HOME/.zshrc ; then
+	echo created symlink: $PWD/.zshrc $HOME/.zshrc
+fi
